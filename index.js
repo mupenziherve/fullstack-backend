984113caa76bf3eb54d9d87b6f2b1869fb1ed14c
Mupenzi Herve <mupenziherve04@gmail.com>
2026-01-27 23:58:31 +0200

    my backend codes using express js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+app.get("/", (req,res)=> {
+    res.send("Server is now running smooth") 
+});
+
+app.listen(5001, () => {
+    console.log();
+})
