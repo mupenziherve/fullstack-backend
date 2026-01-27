d324e15b4269c13aaaeb252629ecf88cc813b2eb
Mupenzi Herve <mupenziherve04@gmail.com>
2026-01-28 00:43:33 +0200

    building a server

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.get("/", (req,res)=> {
-    res.send("Server is now running smooth") 
+
+let tasks = [];
+
+
+app.get("/tasks", (req,res)=> {
+
+res.json(tasks); 
+
 });
+
+app.tasks("/task",(req,res) => {
+    const newTask = {
+        id: Date.now(),
+        title: req.body.title,
+    }
+})
+
+
 
 app.listen(5001, () => {
     console.log("wow it's working");
