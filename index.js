c10df6fb51671b9ed28593fc9ed6475dd90ac89b
Mupenzi Herve <mupenziherve04@gmail.com>
2026-02-08 23:25:12 +0200

    building node app for my datbase

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const
+const express = require("express");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("API is running");
+})
+
+app.listen(5000, ()=>{
+    console.log()
+
+})
