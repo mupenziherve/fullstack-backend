cdedd115aa565b7ec57688fdb252d008effac7c7
Mupenzi Herve <mupenziherve04@gmail.com>
2026-02-12 23:28:44 +0200

    fixing a bug

--- index.js
+++ index.js
@@ -6,4 +6,20 @@ const Task = require("./models/Task");
 const app = express();
 connectDB();
 
+app.use(express.json())const express = require("express");
+const connectDB = require("./config/db");
+
+const Task = require("./models/Task");
+
+const app = express();
+connectDB();
+
+app.use(express.json())const express = require("express");
+const connectDB = require("./config/db");
+
+const Task = require("./models/Task");
+
+const app = express();
+connectDB();
+
 app.use(express.json())
