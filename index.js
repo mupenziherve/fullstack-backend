a6902bce09917a611d09b09cfcba21b436129f38
Mupenzi Herve <mupenziherve04@gmail.com>
2026-02-12 23:31:19 +0200

    duplicate error in Task

--- index.js
+++ index.js
@@ -6,20 +6,4 @@ const Task = require("./models/Task");
 const app = express();
 connectDB();
 
-app.use(express.json())const express = require("express");
-const connectDB = require("./config/db");
-
-const Task = require("./models/Task");
-
-const app = express();
-connectDB();
-
-app.use(express.json())const express = require("express");
-const connectDB = require("./config/db");
-
-const Task = require("./models/Task");
-
-const app = express();
-connectDB();
-
 app.use(express.json())
